16259bd548e4574a744922b6146c96aca2dafd63
pmogan77 <60144163+pmogan77@users.noreply.github.com>
2021-01-06 17:44:08 -0600

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
 
 var PORT = process.env.PORT||3000;
+const positiveScore =  5;
+const negativeScore = 1;
 
+function locate(word, worksheet){
+
+      
+      var rowNum = 2;
+      var foundRow = -1;
+
+      while(worksheet.getRow(rowNum).getCell(1).value!=null)
+      {
+            var data = worksheet.getRow(rowNum).getCell(1).value;
+
+            if(data.toLowerCase()==word.toLowerCase())
+            {
+                  foundRow=rowNum;
+                  break;
+            }
+            
+            rowNum++;
+      }
+
+      return foundRow;
+
+};
 
 const server = http.createServer((req,res) => {
       const pathName = url.parse(req.url,true).pathname;
@@ -21,14 +47,136 @@ const server = http.createServer((req,res) => {
         
       }
       else if(req.method==='DELETE')
-        {
+      {
         
         
         
       }
-
       else{
-            if(pathName==='/'||pathName==='/index.html'||pathName==='/index')
+            if(pathName=='/generalInfo')
+            {
+                  let body = [];
+                  req.on('data', chunk => {
+                        body.push(chunk);
+                  });
+                  req.on('end', () => {
+                        var symptoms = JSON.parse(body.toString());
+                        var severityScores = [];
+                        
+                        workbook.xlsx.readFile('./data/Symptom-severity.xlsx').then(()=>{
+                              var worksheet = workbook.worksheets[0];
+                              
+                              symptoms.forEach(element => {
+                                    
+                                    var rowNum = locate(element,workbook.worksheets[0]);
+
+                                    var severity = worksheet.getRow(rowNum).values[2];
+
+                                    if(severity==undefined||severity==null)
+                                    {
+                                          severity = -1;
+                                    }
+
+                                    severityScores.push(severity);
+                              });
+
+                              //console.log(severityScores); 
+                              
+                              var diseasePrediction = [];
+
+                              workbook.xlsx.readFile('./data/dataset.xlsx').then(()=>{
+                                    var worksheet = workbook.worksheets[0];
+
+                                    var rowNum = 2;
+
+                                    while(worksheet.getRow(rowNum).getCell(1).value!=null)
+                                    {
+                                          var row = worksheet.getRow(rowNum).values;
+
+                                          var rowScore = 0;
+
+                                          var length = row.length;
+
+                                          for(var i = 2;i<length;i++)
+                                          {
+
+                                                if(symptoms.includes(row[i])||symptoms.includes(row[i].substring(1)))
+                                                {
+                                                      rowScore+=positiveScore;
+                                                }
+                                                else{
+                                                      rowScore-=negativeScore;
+                                                }
+                                          }
+
+                                          diseasePrediction.push({condition: row[1], score: rowScore});
+
+                                          rowNum++;
+                                    }
+                                    
+                                    diseasePrediction.sort(function(a,b){return b.score-a.score});
+
+                                    diseasePrediction = diseasePrediction.filter((first, index, self) =>
+                                          index === self.findIndex((second) => (
+                                                //Note: EDIT SIMILARITY CONDITION AS NEEDED
+                                                second.condition === first.condition && second.score === first.score
+                                          ))
+                                    )
+
+
+                                    
+                                    var result = {severityScores: severityScores, diseasePrediction: diseasePrediction};
+      
+                                    res.writeHead(200, {'Content-type': 'application/json'});
+                                    res.end(JSON.stringify(result));
+                              });
+                        });
+
+                        
+                  });
+            }
+            else if(pathName=='/additionalInfo')
+            {
+                  let body = [];
+                  req.on('data', chunk => {
+                        body.push(chunk);
+                  });
+                  req.on('end', () => {
+                        const condition = body.toString();
+
+                        console.log(condition);
+
+                        workbook.xlsx.readFile('./data/symptom_Description.xlsx').then(()=>{
+                              var worksheet = workbook.worksheets[0];
+
+                              var rowNum = locate(condition,worksheet);
+
+                              var diseaseDesc = worksheet.getRow(rowNum).values[2];
+
+                              if(diseaseDesc==null||diseaseDesc==undefined)
+                              {
+                                    diseaseDesc='no record on file';
+                              }
+
+                              workbook.xlsx.readFile('./data/symptom_precaution.xlsx').then(()=>{
+                                    var worksheet = workbook.worksheets[0];
+      
+                                    var rowNum = locate(condition,worksheet);
+      
+                                    var list = worksheet.getRow(rowNum).values;
+
+                                    var result = {diseaseDesc: diseaseDesc, precautions: list.splice(2)};
+
+                                    console.log(result);
+      
+                                    res.writeHead(200, {'Content-type': 'application/json'});
+                                    res.end(JSON.stringify(result));
+                              });
+                        });
+                       
+                  });
+            }
+            else if(pathName==='/'||pathName==='/index.html'||pathName==='/index')
             {
 
                   fs.readFile(startPath+'/index.html', 'utf-8', (err, data) => {
